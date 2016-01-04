c086a0b5d418522207b9f31ca63fc59f3773bc16
jimmy <hu.gw@ikcrm.com>
2016-01-04 23:50:44 +0800

    电动车定位每20秒刷新一次

--- app/src/main/java/com/jcsoft/emsystem/fragment/LocationFragment.java
+++ app/src/main/java/com/jcsoft/emsystem/fragment/LocationFragment.java
@@ -1,21 +1,14 @@
 package com.jcsoft.emsystem.fragment;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdate;
 import com.amap.api.maps.CameraUpdateFactory;
-import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.CameraPosition;
@@ -40,22 +33,13 @@ import org.xutils.x;
 /**
  * Created by jimmy on 15/12/28.
  */
-public class LocationFragment extends BaseFragment implements LocationSource,
-        AMapLocationListener, RadioGroup.OnCheckedChangeListener {
+public class LocationFragment extends BaseFragment implements Runnable {
     @ViewInject(R.id.map_view)
     MapView mapView;
-    @ViewInject(R.id.gps_radio_group)
-    private RadioGroup mGPSModeGroup;
-    @ViewInject(R.id.location_errInfo_text)
-    private TextView mLocationErrText;
     private AMap aMap;
-    private OnLocationChangedListener mListener;
-    private AMapLocationClient mlocationClient;
-    private AMapLocationClientOption mLocationOption;
-    /**
-     * 标志位，标志已经初始化完成
-     */
+    //标志位，标志已经初始化完成
     private boolean isPrepared;
+    private Handler handler = new Handler();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -64,6 +48,7 @@ public class LocationFragment extends BaseFragment implements LocationSource,
         isPrepared = true;
         mapView.onCreate(savedInstanceState);
         init();
+        //第一次定位电动车位置
         requestDatas();
         return view;
     }
@@ -74,10 +59,7 @@ public class LocationFragment extends BaseFragment implements LocationSource,
     private void init() {
         if (aMap == null) {
             aMap = mapView.getMap();
-            setUpMap();
         }
-        mGPSModeGroup.setOnCheckedChangeListener(this);
-        mLocationErrText.setVisibility(View.GONE);
         moveToCenter("36.68445", "117.126229");
     }
 
@@ -103,36 +85,6 @@ public class LocationFragment extends BaseFragment implements LocationSource,
                         Double.valueOf(latlngArr[1])), 17, 0, 0);
         CameraUpdate center = CameraUpdateFactory.newCameraPosition(cp);
         aMap.moveCamera(center);
-    }
-
-    /**
-     * 设置一些amap的属性
-     */
-    private void setUpMap() {
-        aMap.setLocationSource(this);// 设置定位监听
-        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
-        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
-        // 设置定位的类型为定位模式 ，可以由定位、跟随或地图根据面向方向旋转几种
-        aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
-    }
-
-    @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
-        switch (checkedId) {
-            case R.id.gps_locate_button:
-                // 设置定位的类型为定位模式
-                aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
-                break;
-            case R.id.gps_follow_button:
-                // 设置定位的类型为 跟随模式
-                aMap.setMyLocationType(AMap.LOCATION_TYPE_MAP_FOLLOW);
-                break;
-            case R.id.gps_rotate_button:
-                // 设置定位的类型为根据地图面向方向旋转
-                aMap.setMyLocationType(AMap.LOCATION_TYPE_MAP_ROTATE);
-                break;
-        }
-
     }
 
     @Override
@@ -174,67 +126,14 @@ public class LocationFragment extends BaseFragment implements LocationSource,
     }
 
     /**
-     * 定位成功后回调函数
-     */
-    @Override
-    public void onLocationChanged(AMapLocation amapLocation) {
-        if (mListener != null && amapLocation != null) {
-            if (amapLocation != null
-                    && amapLocation.getErrorCode() == 0) {
-                mLocationErrText.setVisibility(View.GONE);
-                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
-            } else {
-                String errText = "定位失败," + amapLocation.getErrorCode() + ": " + amapLocation.getErrorInfo();
-                Log.e("AmapErr", errText);
-                mLocationErrText.setVisibility(View.VISIBLE);
-                mLocationErrText.setText(errText);
-            }
-        }
-    }
-
-    /**
-     * 激活定位
-     */
-    @Override
-    public void activate(OnLocationChangedListener listener) {
-        mListener = listener;
-        if (mlocationClient == null) {
-            mlocationClient = new AMapLocationClient(getActivity());
-            mLocationOption = new AMapLocationClientOption();
-            //设置定位监听
-            mlocationClient.setLocationListener(this);
-            //设置为高精度定位模式
-            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-            //设置定位参数
-            mlocationClient.setLocationOption(mLocationOption);
-            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
-            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
-            // 在定位结束后，在合适的生命周期调用onDestroy()方法
-            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
-            mlocationClient.startLocation();
-        }
-    }
-
-    /**
-     * 停止定位
-     */
-    @Override
-    public void deactivate() {
-        mListener = null;
-        if (mlocationClient != null) {
-            mlocationClient.stopLocation();
-            mlocationClient.onDestroy();
-        }
-        mlocationClient = null;
-    }
-
-    /**
      * 方法必须重写
      */
     @Override
     public void onResume() {
         super.onResume();
         mapView.onResume();
+        //每20秒刷新一次电动车位置
+        handler.postDelayed(this, 1000 * 20);
     }
 
     /**
@@ -244,7 +143,6 @@ public class LocationFragment extends BaseFragment implements LocationSource,
     public void onPause() {
         super.onPause();
         mapView.onPause();
-        deactivate();
     }
 
     /**
@@ -263,8 +161,12 @@ public class LocationFragment extends BaseFragment implements LocationSource,
     public void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
-        if (null != mlocationClient) {
-            mlocationClient.onDestroy();
-        }
+        handler.removeCallbacks(this); //停止刷新
+    }
+
+    @Override
+    public void run() {
+        requestDatas();
+        handler.postDelayed(this, 1000 * 20);// 间隔20秒
     }
 }
